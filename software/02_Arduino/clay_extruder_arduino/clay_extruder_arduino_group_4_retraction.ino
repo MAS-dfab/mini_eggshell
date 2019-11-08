dd4a7044fc5f27e89a262590418edacf5603fa4c
tetov <a@tetov.se>
2019-11-08 15:43:47 +0100

    Updated files, workflow_troubleshooing added, WIP of new arduino code

---
+++ software/02_Arduino/clay_extruder_arduino/clay_extruder_arduino_group_4_retraction.ino
@@ -0,0 +1,42 @@
+// WIP!!!
+
+
+// Stepper library created by Mike McCauley, install from Tools > Manage Libraries
+#include <AccelStepper.h>
+
+//defines pins
+const int stepPin = 6; // PUL - Pulse
+const int dirPin = 7; // DIR - Direction
+const int enPin = 8; // ENA - Enable
+const int urPin = 4; // UR io via relay
+const int MOTOR_SPEED = 1100;
+
+// ===============================================================================
+// STEPPER MOTOR
+// ===============================================================================
+// setSpeed(): Set the speed in steps per second:
+// Driver is set to 200 steps / revolution
+// This means setSpeed(3200) will give 16 revolution / second
+// Create a new instance of the AccelStepper class:
+
+AccelStepper stepper = AccelStepper(1, stepPin, dirPin);
+void rotateStepper(){
+ // Rotates the stepper motor with a desired speed
+  stepper.setSpeed(MOTOR_SPEED);
+}
+
+void setup() {
+  // Set the maximum speed in steps per second:
+  stepper.setMaxSpeed(3200);
+  stepper.runSpeed();
+  pinMode(urPin, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(urPin) == LOW) { //LOW == ur HIGH means io enabled
+    rotateStepper(); // Rotate the stepper motor
+  else {
+    stepper.moveTo(-100)
+    }
+    }
+  }
