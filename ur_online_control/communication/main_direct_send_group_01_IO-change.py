5bc52ce2bd9ea7c7d27640ec6b67385fdf783e6c
edurnemorales <edurne_207@hotmail.com>
2019-11-12 12:35:49 +0100

    ADDED IO CONTROLLER

---
+++ ur_online_control/communication/main_direct_send_group_01_IO-change.py
@@ -0,0 +1,146 @@
+from __future__ import print_function
+from __future__ import absolute_import
+
+import time
+import sys
+import os
+import json
+
+import socket
+
+UR_SERVER_PORT = 30002
+
+# shorcut to run the program from anaconda prompt directly
+# python path to your file....\mini_eggshell\ur_online_control\communication\main_direct_send_group_01.py
+
+# set the paths to find library
+file_dir = os.path.dirname(__file__)
+parent_dir = os.path.abspath(os.path.join(file_dir, "..", ".."))
+sys.path.append(file_dir)
+sys.path.append(parent_dir)
+
+from ur_online_control.communication.formatting import format_commands
+
+# GLOBALS
+# ===============================================================
+server_address = "192.168.10.11"
+server_port = 30003
+ur_ip = "192.168.10.10"
+tool_angle_axis = [-68.7916, -1.0706, 100, 3.1416, 0.0, 0.0]
+# ===============================================================
+
+# COMMANDS
+# ===============================================================
+path = os.path.dirname(os.path.join(__file__))
+filename = os.path.join(path, "..", "commands_group_01_test.json")
+with open(filename, 'r') as f:
+    data = json.load(f)
+# load the commands from the json dictionary
+move_filament_loading_pt = data['move_filament_loading_pt']
+len_command = data['len_command']
+gh_commands = data['gh_commands']
+io_list = data['io_list']
+commands = format_commands(gh_commands, len_command)
+
+# check length commands & stop_extruder
+# if move_filament_loading_pt == True:
+#     if len(stop_extruder) != len(commands)-2:
+#         raise ValueError("Different number of stop_extruder and commands")
+#     elif len(stop_extruder) != len(commands):
+#         raise ValueError("Different number of stop_extruder and commands")
+
+# ===============================================================
+
+# UR SCRIPT
+# ===============================================================
+def movel_commands(server_address, port, tcp, commands, io_list, filament_extruder_motor_DO):
+    script = ""
+    script += "def program():\n"
+    x, y, z, ax, ay, az = tcp
+    script += "\tset_tcp(p[%.5f, %.5f, %.5f, %.5f, %.5f, %.5f])\n" % (x/1000., y/1000., z/1000., ax, ay, az)
+    for i in range(len(commands)):
+        x, y, z, ax, ay, az, speed, radius = commands[i]
+        script += "\tmovel(p[%.5f, %.5f, %.5f, %.5f, %.5f, %.5f], v=%f, r=%f)\n" % (x/1000., y/1000., z/1000., ax, ay, az, speed/1000., radius/1000.)
+        script += "\ttextmsg(\"sending command number %d\")\n" % (i)
+        if io_list[i] == 0:
+            script += "\tset_digital_out(%i, False)\n" % (int(filament_extruder_motor_DO))
+            script += "\ttextmsg(\">> Turn I/O to OFF.\")\n"
+        else:
+            script += "\tset_digital_out(%i, True)\n" % (int(filament_extruder_motor_DO))
+            script += "\ttextmsg(\">> Turn I/O to ON.\")\n"
+    script += "\tsocket_open(\"%s\", %d)\n" % (server_address, port)
+    script += "\tsocket_send_string(\"c\")\n"
+    script += "\tsocket_close()\n"
+    script += "end\n"
+    script += "program()\n\n\n"
+    script = script.encode()
+    return script
+
+# ===============================================================
+def start_extruder(tcp, movel_command, digital_output):
+    script = ""
+    script += "def program():\n"
+    script += "\ttextmsg(\">> Start extruder.\")\n"
+    x, y, z, ax, ay, az = tcp
+    script += "\tset_tcp(p[%.5f, %.5f, %.5f, %.5f, %.5f, %.5f])\n" % (x/1000., y/1000., z/1000., ax, ay, az)
+    x, y, z, ax, ay, az, speed, radius = movel_command
+    script += "\tmovel(p[%.5f, %.5f, %.5f, %.5f, %.5f, %.5f], v=%f, r=%f)\n" % (x/1000., y/1000., z/1000., ax, ay, az, speed/1000., radius/1000.)
+    script += "\tset_digital_out(%i, True)\n" % (int(digital_output))
+    script += "end\n"
+    script += "program()\n\n\n"
+    script = script.encode()
+    return script
+
+# ===============================================================
+def stop_extruder(tcp, movel_command, digital_output):
+    script = ""
+    script += "def program():\n"
+    script += "\ttextmsg(\">> Stop extruder.\")\n"
+    x, y, z, ax, ay, az = tcp
+    script += "\tset_tcp(p[%.5f, %.5f, %.5f, %.5f, %.5f, %.5f])\n" % (x/1000., y/1000., z/1000., ax, ay, az)
+    script += "\tset_digital_out(%i, False)\n" % (int(digital_output))
+    x, y, z, ax, ay, az, speed, radius = movel_command
+    script += "\tmovel(p[%.5f, %.5f, %.5f, %.5f, %.5f, %.5f], v=%f, r=%f)\n" % (x/1000., y/1000., z/1000., ax, ay, az, speed/1000., radius/1000.)
+    script += "end\n"
+    script += "program()\n\n\n"
+    script = script.encode()
+    return script
+
+# ===============================================================
+def main(commands):
+    send_socket = socket.create_connection((ur_ip, UR_SERVER_PORT), timeout=2)
+    send_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # define i/o digital output numbers
+    #air_pressure_DO = 0
+    # clay_extruder_motor_DO = 0
+    #
+    filament_extruder_motor_DO = 1
+    # plastic_extruder_fan_DO = 0
+
+
+    script = movel_commands(server_address, server_port, tool_angle_axis, commands, io_list, filament_extruder_motor_DO)
+    print("sending commands ...")
+
+    # send file
+    send_socket.send(script)
+
+    # make server
+    recv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    recv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Bind the socket to the port
+    recv_socket.bind((server_address, server_port))
+    # Listen for incoming connections
+    recv_socket.listen(1)
+    while True:
+        connection, client_address = recv_socket.accept()
+        print("client_address", client_address)
+        break
+    recv_socket.close()
+
+    send_socket.close()
+    print ("program done ...")
+
+
+if __name__ == "__main__":
+    main(commands)
